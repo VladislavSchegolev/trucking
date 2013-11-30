3c22d110fd8a5f5bd11d7a4d9fbe6f8de739c7ed
Vlad Shchegolev <vladislav.schegolev@gmail.com>
2013-12-01 00:36:19 +0700

    first commit

---
+++ Trucking/Trucking/TRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRAppDelegate.h
+//  Trucking
+//
+//  Created by Vlad Shchegolev on 30/11/13.
+//  Copyright (c) 2013 Vlad Shchegolev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Trucking/TruckingTests/TruckingTests.m
@@ -0,0 +1,34 @@
+//
+//  TruckingTests.m
+//  TruckingTests
+//
+//  Created by Vlad Shchegolev on 30/11/13.
+//  Copyright (c) 2013 Vlad Shchegolev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TruckingTests : XCTestCase
+
+@end
+
+@implementation TruckingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
